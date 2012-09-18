2e9709c71237a70677ee2dad528fd38fa72956da
Mika Boström <bostik@odin.bostik.iki.fi>
2012-09-18 15:40:16 +0300

    Autogenerate GPG key for repository signing

---
+++ apt/create-repo-key.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# There is no way to use aptitude without signing packages any longer.
+# Very nice idea (and one I generally applaud), but using *local*,
+# *non-networked* repositories should be possible without repository
+# signing. Ah well, minor usability corner-cases regularly take back
+# seat when dealing with security issues.
+
+# Only create the key(s) once
+if [ -e /var/tmp/wayland-repo-key.sec ]; then
+    exit 0
+fi
+
+# Create an unencrypted (no passphrase) GPG key
+/usr/bin/gpg --gen-key --batch apt/gpg-key-params
+
