fe0607e91c20683fed680d50746d91ec00e3157a
Mika Boström <bostik@odin.bostik.iki.fi>
2012-09-20 00:22:55 +0300

    Purge libcairo2 as well
    This library has tentacles all over the place.

--- apt/purge-packages.sh
+++ apt/purge-packages.sh
@@ -5,7 +5,7 @@
 
 # Removing these packages clears the entire Wayland development stack
 BASEPKGS="libglapi-mesa mesa-common-dev libgl1-mesa-dri libdrm2 "
-BASEPKGS+="libxkbcommon0 libkms1 libegl1-mesa-drivers"
+BASEPKGS+="libxkbcommon0 libkms1 libegl1-mesa-drivers libcairo2"
 
 # Remove everything
 sudo aptitude -y purge ${BASEPKGS}
