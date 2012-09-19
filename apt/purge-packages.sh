73aa8de6c7d8dfcc56463e102729a32a22fb878c
Mika Boström <bostik@odin.bostik.iki.fi>
2012-09-19 14:05:09 +0300

    Add script to purge the entire stack
    When starting the whole build, it's necessary to remove all relevant
    packages.

---
+++ apt/purge-packages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Bash? The '+=' syntax for appending to variable is not supported in
+# plain old /bin/sh.
+
+# Removing these packages clears the entire Wayland development stack
+BASEPKGS="libglapi-mesa mesa-common-dev libgl1-mesa-dri libdrm2 "
+BASEPKGS+="libxkbcommon0"
+
+# Remove everything
+sudo aptitude purge ${BASEPKGS}
