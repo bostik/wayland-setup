251c1b1e9bbcb17d4173bea4112b44302d89796b
Mika Boström <bostik@odin.bostik.iki.fi>
2012-09-22 09:08:53 +0300

    Purge even more packages before build cycle
    The new mesa packages may have imperfect dependencies.

--- apt/purge-packages.sh
+++ apt/purge-packages.sh
@@ -5,7 +5,8 @@
 
 # Removing these packages clears the entire Wayland development stack
 BASEPKGS="libglapi-mesa mesa-common-dev libgl1-mesa-dri libdrm2 "
-BASEPKGS+="libxkbcommon0 libkms1 libegl1-mesa-drivers libcairo2"
+BASEPKGS+="libxkbcommon0 libkms1 libegl1-mesa-drivers libcairo2 "
+BASEPKGS+="libegl1-mesa libegl1-mesa-drivers"
 
 # Remove everything
 sudo aptitude -y purge ${BASEPKGS}
